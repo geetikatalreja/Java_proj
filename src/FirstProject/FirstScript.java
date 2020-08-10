f8ce00a788c054d16f32dc8e375bf41570bd6fb2
geetikatalreja <geetika.talreja@nagarro.com>
2020-08-10 11:48:29 +0530

    dcd

--- src/FirstProject/FirstScript.java
+++ src/FirstProject/FirstScript.java
@@ -3,7 +3,7 @@ public class FirstScript {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello1");
+		System.out.println("Hello12");
 
 	}
 
